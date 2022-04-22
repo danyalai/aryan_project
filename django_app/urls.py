305dfbb34279855672abceb5b2266a1741bd3dec
aryan <aryantahami@gmail.com>
2022-04-22 11:23:29 +0430

    add httpresponse

---
+++ django_app/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from  . import views
+urlpatterns = [
+    path('http/', views.aryan_http),
+]
--- django_app/views.py
+++ django_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def aryan_http(request):
+    return HttpResponse('i am in your app')
+    
